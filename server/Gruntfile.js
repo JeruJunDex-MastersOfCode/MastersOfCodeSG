a071dd07fcbd16127b4a3c93285405e2968e08e1
Dexter Brylle Matos <dbrylle.dlscsb@gmail.com>
2015-03-28 13:58:20 +0800

    Server side configs

---
+++ server/Gruntfile.js
@@ -0,0 +1,4 @@
+module.exports = function (grunt) {
+	var NG_APP_ROOT = '../client-side/app';
+	var PORT = process.env.PORT || (3000);
+};
---
+++ server/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+	db: {
+		server: 'admin:MastersOfCode0800@ds035438.mongolab.com:35438',
+		name: 'mastersofcode'
+	},
+	simplifyCommerce: {
+		api_key: '',
+		api_secret: ''
+	},
+	moneySend: {
+
+	},
+	app: {
+		cookieSecret: '',
+		name: '',
+	}
+}
+
+mongodb:///mastersofcode
