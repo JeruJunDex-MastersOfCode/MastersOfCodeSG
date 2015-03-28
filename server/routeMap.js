3d5dd9d35365b9013db2ae91c16a9ae7c062be1a
JeruInTheBox <jerugasendo@gmail.com>
2015-03-28 17:21:00 +0800

    Added signup and getallusers routes

--- server/routeMap.js
+++ server/routeMap.js
@@ -1,11 +1,5 @@
 module.exports = [
-	['/api', 'index#index', 'get']
-	/*['/auth/signup_mom', 'auth#signupMom', 'post'],
-	['/api/users/moms/signup', 'users#addMom', 'post'],
-	['/api/users', 'users#index', 'get'],
-	['/api/users/moms', 'users#getAllMoms', 'get']
-	['/api/users/moms/:mom_id', 'users#getOneMom', 'get'],
-	['/api/users/moms']
-	['/api/users/signup_va', 'auth#signupVa', 'post'],
-	*/
+	['/api', 'index#index', 'get'],
+	['/api/users/signup', 'users#signup', 'post'],
+	['/api/users/getAllUsers', 'users#getAllUsers', 'get']
 ];
