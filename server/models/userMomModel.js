0e56a45f742a54164e95d38aa6cc3c849d85c61c
Dexter Brylle Matos <dbrylle.dlscsb@gmail.com>
2015-03-28 13:57:43 +0800

    Initial commit of db schema.

---
+++ server/models/userMomModel.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose'),
+	Schema = mongoose.Schema;
+var ObjectId = mongoose.Schema.Types.ObjectId;
+
+module.exports = function () {
+
+	this.collection = 'users';
+
+	this.schema = mongoose.Schema({
+
+	});
+}
