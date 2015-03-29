a875d9192bfa68d5fff7ed905a0b97b067e1629f
Dexter Brylle Matos <dbrylle.dlscsb@gmail.com>
2015-03-29 09:48:24 +0800

    Configured endpoints

--- server/routeMap.js
+++ server/routeMap.js
@@ -1,13 +1,13 @@
 module.exports = [
-    ['/api', 'index#index', 'get'],
-    ['/api/users/signup', 'users#signup', 'post'],
-    ['/api/users/getAllUsers', 'users#getAllUsers', 'get'],
-    ['/api/users/:uid/editProfile', 'users#editProfile', 'post'],
-    ['/api/users/:uid', 'users#getUser', 'get'], 
-    ['/api/tasks/getAllTasks', 'tasks#getAllTasks', 'get'],    
-    ['/api/tasks/:taskId', 'tasks#getTask', 'get'],
-    ['/api/tasks/addTask', 'tasks#addTask', 'post'],    
-    ['/api/testRoute', 'users#testRoute', 'get'],
-    ['/api/testPayment', 'users#testPayment', 'get']
-    //['/api/users/getAllUsers', 'users#getAllUsers', 'get']
+	['/api', 'index#index', 'get'],
+	['/api/users/signup', 'users#signup', 'post'],
+	['/api/users/getAllUsers', 'users#getAllUsers', 'get'],
+	['/api/users/:uid/editProfile', 'users#editProfile', 'post'],
+	['/api/users/:uid', 'users#getUser', 'get'], 
+	['/api/tasks/getAllTasks', 'tasks#getAllTasks', 'get'],    
+	['/api/tasks/:taskId', 'tasks#getTask', 'get'],
+	['/api/tasks/addTask', 'tasks#addTask', 'post'],
+	['/api/users/getSimplifyCustomer', 'users#getSimplifyCustomer', 'post'],
+	['/api/transactions/checkout', 'transactions#checkout', 'post'],
+	// ['/api/transactions/testone', 'transactions#test', 'post'],
 ];
